f6d116720387509e727a1057be36fed19b6a656e
Admin-Brent <bottke7@hotmail.com>
2019-06-12 15:50:28 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,16 +31,27 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards.rank);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
 
 
 
